3e154512ec0051dbb20a9c2e5a0463973ed7e927
Yusuf-Zw <yousufzwz@gmail.com>
2023-09-09 00:54:31 +0600

    code committed v.1

---
+++ Problem_Solutions/Problem-1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+string inputString = Console.ReadLine(); //mm#al#hmws##ud
+
+Stack<char> stk = new ();
+for (int i = 0; i < inputString.Length; i++)
+{
+    if (inputString[i] == '#')
+    {
+        stk.Pop();
+    }
+    else
+    {
+        stk.Push(inputString[i]);
+    }
+}
+
+PrintStringFromStack(stk);
+//while (stk.Count > 0)
+//{
+//    Console.Write(stk.Peek());
+//    stk.Pop();
+//}
+
+
+void PrintStringFromStack(Stack<char> stk)
+{
+    if (stk.Count == 0)
+    {
+        return;
+    }
+
+    char element = stk.Peek();
+    stk.Pop();
+    PrintStringFromStack(stk);
+    Console.Write(element);
+}
