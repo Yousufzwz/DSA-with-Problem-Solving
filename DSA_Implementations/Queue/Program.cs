15a8e6f238c6e444b996651c7ec4e535bbb51dce
Yusuf-Zw <yousufzwz@gmail.com>
2023-09-09 22:03:00 +0600

    code committed v.2

---
+++ DSA_Implementations/Queue/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Queue<int> elements = new();
+elements.Enqueue(1);
+elements.Enqueue(2);
+elements.Enqueue(3);
+Console.WriteLine($"First Element:{elements.Peek()}");
+Console.WriteLine($"Size:{elements.Count()}");
+elements.Dequeue();
+Console.WriteLine($"After removing first element, now first Element:{elements.Peek()}");
---
+++ DSA_Implementations/Stack/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Stack<int> elements = new();
+elements.Push(1);
+elements.Push(2);
+elements.Push(3);
+Console.WriteLine($"Top Element:{elements.Peek()}");
+Console.WriteLine($"Size:{elements.Count()}");
+elements.Pop();
+Console.WriteLine($"After removing top element, now top Element:{elements.Peek()}");
