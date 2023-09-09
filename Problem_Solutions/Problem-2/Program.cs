34f1f0768d17be0290462f0bf2e80f90fa14f92e
Yusuf-Zw <yousufzwz@gmail.com>
2023-09-10 00:35:34 +0600

    code committed v.3

---
+++ Problem_Solutions/Problem-2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//Parentheses Balance (UVA_673)
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string s = Console.ReadLine();
+    Console.WriteLine(IsBalanced(s)? "Yes":"No");
+}
+
+bool IsBalanced(string s)
+{
+    Stack<char> stk= new ();
+
+    foreach(char c in s)
+    {
+        if (c == '(' ||  c == '[')
+        {
+            stk.Push (c);   
+        }
+        else if(c == ')' && (stk.Count == 0 || stk.Pop() != '('))
+        {
+            return false;
+        }
+        else if (c == ']' && (stk.Count == 0 || stk.Pop() != '['))
+        {
+            return false;
+        }
+    }
+    return stk.Count == 0;
+}
