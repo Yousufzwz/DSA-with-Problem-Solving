f72bb06eb05c3605297d0fb7c3926f645754c5d1
Yusuf-Zw <yousufzwz@gmail.com>
2023-09-12 22:14:52 +0600

    code committed v.4

---
+++ Problem_Solutions/Problem-3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Palindrome Check
+
+bool IsPalindrome(string text)
+{
+    string solidInput = text.Replace(" ",string.Empty).ToLower();
+    int leftCounter = 0; 
+    int rightCounter = solidInput.Length -1 ;
+
+    while(leftCounter < rightCounter)
+    {
+        if (solidInput[leftCounter] != solidInput[rightCounter])
+        {
+            return false;
+        }
+        leftCounter++;
+        rightCounter--;
+    }
+    return true;
+
+}
+
+string textInput= Console.ReadLine();
+if(IsPalindrome(textInput))
+{
+    Console.WriteLine("Palindrome!");
+}
+else
+{
+    Console.WriteLine("Not Palindrome!");
+}
