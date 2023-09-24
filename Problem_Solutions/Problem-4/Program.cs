a6fc9bb865295173ffddadf97fcb6a464b36f969
Yusuf-Zw <yousufzwz@gmail.com>
2023-09-24 21:10:48 +0600

    code committed v.4

--- Problem_Solutions/Problem-4/Program.cs
+++ Problem_Solutions/Problem-4/Program.cs
@@ -1,7 +1,19 @@
 ﻿
 
-string text = Console.ReadLine();
-for(int i =0; i<text.Length; i++)
+string ReverseString(string input)
 {
+    char[] inputChar = new char[input.Length];
+    int length = input.Length;
 
+    for(int i = 0; i < length; i++)
+    {
+        inputChar[i] = input[length - 1 -i];
+    }
+
+    return new string(inputChar);
 }
+
+Console.Write("Enter input string:");
+string inp =Console.ReadLine();
+string reversedString = ReverseString(inp);
+Console.WriteLine($"Reversed String:{reversedString}");
